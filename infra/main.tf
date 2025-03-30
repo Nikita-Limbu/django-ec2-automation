6372a78e9b6adf8a997253065c3bb613472d125e
Nikita-Limbu <nikkilimbu2317@gmail.com>
2025-03-30 17:27:34 +0530

    Fixed SSH authentication by removing remote-exec provisioner

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,6 @@ resource "aws_security_group" "ec2_sg" {
   name        = "django-sg"
   description = "Allow SSH and HTTP access"
   
-  # Allow SSH access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -15,7 +14,6 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow HTTP access
   ingress {
     from_port   = 8000
     to_port     = 8000
@@ -23,7 +21,6 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -38,30 +35,13 @@ resource "aws_security_group" "ec2_sg" {
 
 # EC2 instance configuration
 resource "aws_instance" "django_instance" {
-  ami             = "ami-0e670eb768a5fc3d4"   # Replace with latest AMI
+  ami             = "ami-023a307f3d27ea427"   # latest AMI
   instance_type   = "t3.nano"
-  key_name        = "django-hello-world-key"              # AWS key pair name
-  security_groups = [aws_security_group.ec2_sg.name]      # Reference the SG properly
+  key_name        = "django-hello-world-key"   # Use your AWS key pair name
+  security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
     Name = "DjangoHelloWorld"
-  }
-
-  # Provisioning commands
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y docker.io docker-compose",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo docker run -d -p 8000:8000 nikitalimbu/helloworld-django:latest"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      host        = self.public_ip
-    }
   }
 }
 
