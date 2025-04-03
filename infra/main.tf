c3687cd8cf61d151e8c5ac87fa6a4e698641dd4e
Nikita-Limbu <nikkilimbu2317@gmail.com>
2025-04-03 11:04:20 +0530

    Updated deployment pipeline, main.tf and .gitignore to exclude Terraform state files

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_security_group" "ec2_sg" {
   name        = "django-sg-auto"
   description = "Allow SSH and HTTP access"
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -33,15 +33,27 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-# EC2 instance configuration
+# Automatically create and deploy a new EC2 instance
 resource "aws_instance" "django_instance" {
-  ami             = "ami-023a307f3d27ea427"   # latest AMI
+  ami             = "ami-023a307f3d27ea427"   # Use an appropriate AMI for your region
   instance_type   = "t3.nano"
-  key_name        = "django-hello-world-key"   # Use your AWS key pair name
-  security_groups = [aws_security_group.ec2_sg.name]
+  key_name        = "django-hello-world-key"  # Your existing key pair
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  # This script runs when the instance starts
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y python3 python3-pip
+    sudo apt install -y git
+    git clone https://github.com/Nikita-Limbu/django-ec2-automation.git /home/ubuntu/django-app
+    cd /home/ubuntu/django-app
+    pip3 install -r requirements.txt
+    nohup python3 manage.py runserver 0.0.0.0:8000 &
+  EOF
 
   tags = {
-    Name = "DjangoHelloWorld"
+    Name = "DjangoHelloWorld-Auto"
   }
 }
 
